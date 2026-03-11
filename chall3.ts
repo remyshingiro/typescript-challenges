5822ac11427319a180966cd570599d029ebde18e
Remy <shingiroremy303@gmail.com>
2026-03-11 17:36:10 +0200

    chall3

---
+++ chall3.ts
@@ -0,0 +1,17 @@
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+}
+
+let product1: Product = {
+    name: "Milk",
+    price: 1000,
+    quantity: 10,
+}
+
+let product2: Product = {
+    name: "Chocolate",
+    price: 2000,
+    quantity: 5,
+}

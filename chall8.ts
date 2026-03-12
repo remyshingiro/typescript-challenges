fcfafd772850f5a86b18b1de7b792ddb42852eab
Remy <shingiroremy303@gmail.com>
2026-03-12 16:00:26 +0200

    .

--- chall8.ts
+++ chall8.ts
@@ -22,3 +22,18 @@ interface DocumentationTask {
 
 type Task = CodingTask | TeskingTask | DocumentationTask;
 
+const formatTaskSummary = (task: Task): string => {
+    switch(task.type){
+        case 'coding':
+            return `Coding task #${task.id} in ${task.language}: <${task.status}>`;
+        case 'testing':
+            return `Testing task #${task.id} with ${task.framework}: ${task.testCases} test cases, status: ${task.status}`;
+        case 'doc':
+            return `Documentation task ${task.id} (${task.format}): ${task.pages} pages, status: ${task.status}`;
+    }   
+}
+
+function filterTasksByStatus<T extends Task>(tasks: T[], status: string): T[] {
+    return tasks.filter(task => task.status === status);
+    
+}

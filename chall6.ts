a913ce809598e9eed72f673d65bf3b25d508b279
Remy <shingiroremy303@gmail.com>
2026-03-12 11:17:52 +0200

    implemented chat func | checked logics with switch cases

--- chall6.ts
+++ chall6.ts
@@ -14,3 +14,23 @@ interface VideoMessage {
 }
 
 type ChatMessage = TextMessage | Imagemessage | VideoMessage;
+
+const formatMessage = (chat: ChatMessage): string => {
+    switch(chat.type) {
+        case "text":
+            return `Text: ${chat.content}`;
+        case "image":
+            return `Image [ ${chat.caption}]: ${chat.url}`;
+        case "video":
+            return `Video (${chat.duration}s) + ${chat.url}`;
+    }
+}
+
+const firstCall = formatMessage({ type: "text", content: "Hello, world!" });
+const secondCall = formatMessage({ type: "image", url: "<https://example.com/image.jpg>", caption: "Sunset" });
+const thirdCall = formatMessage({ type: "video", url: "<https://example.com/video.mp4>", duration: 120 });
+
+console.log(firstCall)
+console.log(secondCall)
+console.log(thirdCall)
+
